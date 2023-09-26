6dc3e040089a3a7f97e69aae09637d4bec38a9c9
bharadwaj-o <145960166+bharadwaj-o@users.noreply.github.com>
2023-09-27 01:44:23 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,8 +1,19 @@
 input_string=str(input("Enter the encoded string:"))
-string_split=input_string.split("000")
-print(string_split[0])
+strspl=list()
+str1=""
+strspl=list()
+for i in input_string:
+    if i!="0":
+        str1=str1+i
+    elif input_string[input_string.index(i)-1]!=0:
+        if str1!="":
+            strspl.append(str1)
+        else:
+            continue
+        str1=""
+    elif input_string[input_string.index(i)-1]==0:
+        str1=""
+strspl.append(str1)
 
-dict1=dict({"first_name": string_split[0], "last_name": string_split[1], "id": string_split[2]})
-
-
+dict1=dict({"first_name": strspl[0], "last_name": strspl[1], "id": strspl[2]})
 print(dict1)
